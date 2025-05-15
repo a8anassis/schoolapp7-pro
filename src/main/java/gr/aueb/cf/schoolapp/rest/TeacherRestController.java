acf86f77762e8160d7437609fa30ba92ea2c2b12
a8anassis <a8anassis@gmail.com>
2025-05-15 20:09:49 +0300

    feat: add completed controller

--- src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
+++ src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
@@ -1,9 +1,8 @@
 package gr.aueb.cf.schoolapp.rest;
 
 import gr.aueb.cf.schoolapp.core.exceptions.*;
-import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherUpdateDTO;
+import gr.aueb.cf.schoolapp.dto.*;
+import gr.aueb.cf.schoolapp.mapper.Mapper;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
 import gr.aueb.cf.schoolapp.validator.ValidatorUtil;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -13,11 +12,11 @@ import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
-import jdk.security.jarsigner.JarSigner;
 import lombok.RequiredArgsConstructor;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
@@ -70,5 +69,75 @@ public class TeacherRestController {
                 .entity(readOnlyDTO).build();
     }
 
+    @DELETE
+    @Path("/{teacherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteTeacher(@PathParam("teacherId") Long teacherId)
+            throws EntityNotFoundException {
 
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.getTeacherById(teacherId);
+        teacherService.deleteTeacher(teacherId);
+        return Response
+                .status(Response.Status.OK)
+                .entity(readOnlyDTO)
+                .build();
+    }
+
+    @GET
+    @Path("/{teacherId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTeacher(@PathParam("teacherId") Long id)
+            throws EntityNotFoundException {
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.getTeacherById(id);
+        return Response
+                .status(Response.Status.OK)
+                .entity(readOnlyDTO)
+                .build();
+    }
+
+
+    @GET
+    @Path("/filtered")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFiltered(@QueryParam("firstname") @DefaultValue("") String firstname,
+                                @QueryParam("lastname") @DefaultValue("") String lastname,
+                                @QueryParam("vat") @DefaultValue("") String vat) {
+        TeacherFiltersDTO filtersDTO = new TeacherFiltersDTO(firstname, lastname, vat);
+        Map<String , Object> criteria;
+
+        criteria = Mapper.mapToCriteria(filtersDTO);
+        List<TeacherReadOnlyDTO> readOnlyDTOS = teacherService.getTeachersByCriteria(criteria);
+        return Response.status(Response.Status.OK)
+                .entity(readOnlyDTOS)
+                .build();
+    }
+
+
+    public PaginatedResult<TeacherReadOnlyDTO> getFilteredPaginated(
+            @QueryParam("firstname") @DefaultValue("") String firstname,
+            @QueryParam("lastname") @DefaultValue("") String lastname,
+            @QueryParam("vat") @DefaultValue("") String vat,
+            @QueryParam("page") @DefaultValue("") Integer page,
+            @QueryParam("size") @DefaultValue("") Integer size
+    )
+            throws EntityInvalidArgumentException {
+        TeacherFiltersDTO filtersDTO = new TeacherFiltersDTO(firstname, lastname, vat);
+        Map<String , Object> criteria;
+
+        criteria = Mapper.mapToCriteria(filtersDTO);
+
+        if (page < 0) throw new EntityInvalidArgumentException("PageInvalidNumber", "Invalid page number");
+        if (size <= 0) throw new EntityInvalidArgumentException("SizeInvalidNumber", "Invalid size number");
+
+        List<TeacherReadOnlyDTO> readOnlyDTOS = teacherService.getTeachersByCriteriaPaginated(criteria, page, size);
+        long totalItems = teacherService.getTeachersCountByCriteria(criteria);
+        int totalPages = (int) Math.ceil((double) totalItems / size);
+        return new PaginatedResult<>(
+                readOnlyDTOS,
+                page,
+                size,
+                totalPages,
+                totalItems
+        );
+    }
 }
